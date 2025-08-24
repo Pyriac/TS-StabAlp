de49414d2c8aefae92d7dc96cc139d5061566c25
Pyriac <cyriacpineau@gmail.com>
2025-08-24 12:29:37 +0200

    page Contact provisoire

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,4 +1,22 @@
 export default function Contact() {
-    return <h2>coucou from contact</h2>;
+    return (
+  <section>
+      <h2>Un module de contact par mail est en cours de création</h2>
+      <p>
+        Vous pouvez me contacter par téléphone au{" "}
+        <a className="phone phone-black" href="tel:0686534205">
+          06.86.53.42.05
+        </a>
+      </p>
+      <p>
+        ou par mail à{" "}
+        <a className="email email-black"
+          href="mailto:laurent.pineau73@gmail.com"
+        >
+          laurent.pineau73@gmail.com
+        </a>
+      </p>
+    </section>
+    )
   }
   
